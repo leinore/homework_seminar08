405d031ee29feb70b1ac0a57385fc8a4d6a7a587
leinore <116540768+leinore@users.noreply.github.com>
2022-12-12 18:06:47 +0300

    task_54

---
+++ task_54/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 54: Задайте двумерный массив. Напишите программу, 
+// которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+int[,] FillMatrix(int rows, int columns)
+{
+    int[,] matrix = new int [rows, columns];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = new Random().Next(0,10);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i,j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+int [,] matrix = FillMatrix(m,n);
+PrintMatrix(matrix);
+Console.WriteLine();
+SortedMatrix(matrix);
+PrintMatrix(matrix);
+
+void SortedMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        for (int k = 0; k < matrix.GetLength(1) - 1; k++)
+        {
+        if (matrix[i, k] < matrix[i, k + 1])
+        {
+            int temp = matrix[i, k + 1];
+            matrix[i, k + 1] = matrix[i, k];
+            matrix[i, k] = temp;
+        }
+        }
+    }
+    }
+}
